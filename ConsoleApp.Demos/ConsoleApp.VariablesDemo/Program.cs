36ec1bc62793e9d743feecaeeb00f660553513a8
Benedict Castro <bcastro@servingintel.com>
2024-02-01 16:52:41 +0800

    Added a new project exploring variables in C#

---
+++ ConsoleApp.Demos/ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different data types
+
+/*
+    text        -   str
+    integers    -   int
+    decimal     -   double, float, decimal
+    boolean     -   bool
+*/
+
+string firstName = "Benedict";
+string lastName = "Castro";
+
+Console.WriteLine($"My name is {firstName} {lastName}.");
