911c4a56cfcfd40d1a635cc86a4f00df42bcccf8
benicastro <benedict.zcastro@gmail.com>
2024-02-02 01:33:59 +0800

    Explored new user input features of C# and tried different debugging techniques

--- ConsoleApp.Demos/ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.Demos/ConsoleApp.UserInputDemo/Program.cs
@@ -1,15 +1,39 @@
-﻿// Declare variables
+﻿//Variable Declarations and Types
 
-string name = string.Empty;
-double yourHeight;
+string? firstName = string.Empty;
+string lastName = string.Empty;
+int age;
+int retirementAge = 65;
+decimal salary;
+char gender = char.MinValue;
+bool working = true;
 
 // Prompt the user for input
+Console.Write("Please enter your first name: ");
+firstName = Console.ReadLine();
 
-Console.Write("Please enter your height (m): ");
-yourHeight = Convert.ToDouble(Console.ReadLine());
+Console.Write("Please enter your last name: ");
+lastName = Console.ReadLine();
 
+Console.Write("Please enter your age: ");
+age = Convert.ToInt32(Console.ReadLine());
 
-// Process the data
+Console.Write("Please enter your salary: ");
+salary = Convert.ToDecimal(Console.ReadLine());
 
-int heightCm = (int)yourHeight * 100;
-Console.WriteLine($"Your height is {heightCm} cm.");
+Console.Write("Please enter your gender (M or F): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Are you working? (true or false): ");
+working = Convert.ToBoolean(Console.ReadLine());
+
+// Process the data 
+int workingYearsRemaining = retirementAge - age;
+
+// Output the results to the user
+Console.WriteLine($"Full name: {firstName} {lastName}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Your Salary is: {salary}");
+Console.WriteLine($"Your Gender is: {gender}");
+Console.WriteLine($"You Are Employed: {working}");
+Console.WriteLine($"Number of working years remaining: {workingYearsRemaining}");
