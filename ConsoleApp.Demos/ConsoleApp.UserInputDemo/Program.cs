17f9eafd170e7c200a648ff425e38739d9c7bbdc
benicastro <benedict.zcastro@gmail.com>
2024-02-01 17:53:15 +0800

    Created a new project exploring user inputs in C#

---
+++ ConsoleApp.Demos/ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declare variables
+
+string name = string.Empty;
+double yourHeight;
+
+// Prompt the user for input
+
+Console.Write("Please enter your height (m): ");
+yourHeight = Convert.ToDouble(Console.ReadLine());
+
+
+// Process the data
+
+int heightCm = (int)yourHeight * 100;
+Console.WriteLine($"Your height is {heightCm} cm.");
