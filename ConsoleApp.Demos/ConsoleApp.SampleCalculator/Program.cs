8b2052f0c586378d5de962118afb53aee54e121a
benicastro <benedict.zcastro@gmail.com>
2024-02-08 00:02:28 +0800

    Built a simple calculator project

---
+++ ConsoleApp.Demos/ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,113 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Variable Declarations
+int choice = 0;
+int num1, num2 = 0;
+
+// Show calculator options / Show menu
+while (choice != -1)
+{
+    try
+    {
+        // Welcome Message
+        PrintMenu();
+
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == -1)
+        {
+            break;
+        }
+
+        Console.Write("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        /* Switch statement */
+        int answer = 0;
+        switch (choice)
+        {
+            case 1:
+                answer = Sum(num1, num2);
+                break;
+            case 2:
+                answer = Difference(num1, num2);
+                break;
+            case 3:
+                answer = Product(num1, num2);
+                break;
+            case 4:
+                answer = Quotient(num1, num2);
+                break;
+            case 5:
+                answer = Fibonnaci(num1, num2);
+                break;
+            default:
+                throw new Exception("Invalid Menu Item Selected.");
+        }
+
+        // print output 
+        Console.WriteLine($"The result is: {answer}");
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Cannot divide by zero.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Press enter to continue.");
+        Console.ReadLine();
+    }
+}
+
+Console.WriteLine("******** - Thank you for using the sample calculator! - ********");
+
+
+// Method Definitions
+int Sum(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int Difference(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int Product(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int Quotient(int num1, int num2)
+{
+    return num1 / num2;
+}
+
+int Fibonnaci(int num1, int num2)
+{
+    var answer = 0;
+    for (int i = num1; i <= num2; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
+void PrintMenu()
+{
+    Console.Clear();
+    Console.WriteLine("******** - Welcome to the sample calculator! - ********");
+    Console.WriteLine("Please select an operation (-1 to exit program):");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Fibonacci sequence");
+    Console.Write("Choice: ");
+}
